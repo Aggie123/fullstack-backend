97992d1826a46026a4bace01a959ff4584e0c353
nana <nana.sunshine@qq.com>
2018-11-16 18:26:00 +0800

    server init

--- model/db.js
+++ model/db.js
@@ -1,4 +1,4 @@
-// mongoose 链接
+/*// mongoose 链接
 // 
 var mongoose = require('mongoose');
 //连接相应数据库，在这里连接的是student_dormitory数据库
@@ -15,5 +15,28 @@ db.on('error', function(error) {
 db.once('open', function(error) {
     console.log("数据库连接成功");
 });
+
+module.exports = db;*/
+
+const mongoose = require("mongoose");
+
+const API_PORT = 3001;
+
+// this is our MongoDB database
+// const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+const dbRoute = "mongodb://admin:admin2018@ds161426.mlab.com:61426/local_library"
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+
+const db=require('./model/db');
 const Data = require("./data");
 
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
-
-// this is our MongoDB database
-// const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
-const dbRoute = "mongodb://admin:admin2018@ds161426.mlab.com:61426/local_library"
-
-// connects our back end code with the database
-mongoose.connect(
-  dbRoute,
-  { useNewUrlParser: true }
-);
-
-let db = mongoose.connection;
-
-db.once("open", () => console.log("connected to the database"));
-
-// checks if connection with the database is successful
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
